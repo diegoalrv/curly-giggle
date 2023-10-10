47aae5b1b96eaf39ae88fcbfaf2029f38b5a617e
kevinsmoke21 <kmirandaop.sa@gmail.com>
2023-10-10 14:00:41 -0300

    Pagina web que recibe el json de la api. y la muestra en agradecimiento

---
+++ Kevin/extraer_un_archivoJSON.js
@@ -0,0 +1,20 @@
+// En una aplicacion WEB los datos JSON se reciben como una cadena 
+
+const cars = `[
+    {
+        "modelo": "ford mustang",
+        "produccion":2010,
+        "millaje": 12000
+    },
+    {
+        "modelo": "honda accord",
+        "produccion":2021,
+        "millaje": 4560
+    },
+    {
+        "modelo": "volkswagen Golf",
+        "produccion":2016,
+        "millaje": 58200
+    }
+]`;
+console.log(C)
